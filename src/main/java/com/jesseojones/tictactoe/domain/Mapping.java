60954305c1949e15c538437a3b677c52fd2f7889
jesseojones <jesseoj98@gmail.com>
2022-12-05 09:05:20 -0600

    integer to coordinate mapping method

--- src/main/java/com/jesseojones/tictactoe/domain/Mapping.java
+++ src/main/java/com/jesseojones/tictactoe/domain/Mapping.java
@@ -1,5 +1,24 @@
 package com.jesseojones.tictactoe.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Mapping {
+
+	public Map<Integer, Coordinate> integerToCordinate() {
+		final Map<Integer, Coordinate> map = new HashMap<>();
+
+		map.put(0, new Coordinate(0, 0));
+		map.put(1, new Coordinate(1, 0));
+		map.put(2, new Coordinate(2, 0));
+		map.put(3, new Coordinate(0, 1));
+		map.put(4, new Coordinate(1, 1));
+		map.put(5, new Coordinate(2, 1));
+		map.put(6, new Coordinate(0, 2));
+		map.put(7, new Coordinate(1, 2));
+		map.put(8, new Coordinate(2, 2));
+
+		return map;
+	}
 
 }
