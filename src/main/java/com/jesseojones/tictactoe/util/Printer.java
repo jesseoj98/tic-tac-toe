d72e1bb0cabc48e5c35724c8d53ab44e3d433721
jesseojones <jesseoj98@gmail.com>
2022-12-05 09:36:46 -0600

    printer class documentation

--- src/main/java/com/jesseojones/tictactoe/util/Printer.java
+++ src/main/java/com/jesseojones/tictactoe/util/Printer.java
@@ -1,13 +1,23 @@
 package com.jesseojones.tictactoe.util;
 
+/**
+ * Printer class defines several functions to help with printing state of game
+ * boards
+ */
 public class Printer {
 
+	/** The constant for a game board dimension */
 	private static final int GAME_BOARD_DIMENSIONS = 3;
 
+	/** The constant for the number of game board spaces */
 	private static final int GAME_BOARD_SPACES = 9;
 
+	/** Instantation of helper class */
 	private static Helper helper = new Helper();
 
+	/**
+	 * Prints a simple sample coordinates game board
+	 */
 	public void printSimpleCoordinateSampleGameBoard() {
 		System.out.println("\nSimple Game Board");
 		System.out.println("\nEnter a single digit to place your move\n");
@@ -26,6 +36,11 @@ public class Printer {
 		}
 	}
 
+	/**
+	 * Prints a simple coordinates game board
+	 * 
+	 * @param gameBoard the simple game board
+	 */
 	public void printSimpleCoordinateGameBoard(char[] gameBoard) {
 		printSimpleCoordinateGameBoardDivider();
 		for (int i = 0; i < GAME_BOARD_SPACES; i++) {
@@ -42,6 +57,11 @@ public class Printer {
 		}
 	}
 
+	/**
+	 * Prints a coordinates game board
+	 * 
+	 * @param gameBoard the coordinates game board
+	 */
 	public void printCoordinatesGameBoard(char[][] gameBoard) {
 		printTopOfCoordinatesGameBoard();
 		for (int i = 0; i < GAME_BOARD_DIMENSIONS; i++) {
@@ -53,6 +73,12 @@ public class Printer {
 		printBottomOfCoordinatesGameBoard();
 	}
 
+	/**
+	 * Prints a game board row
+	 * 
+	 * @param gameBoard the coordinates game board
+	 * @param rowNumber the row number
+	 */
 	private void printGameBoardRow(char[][] gameBoard, int rowNumber) {
 		System.out.print((rowNumber + 1) + " |");
 		for (int i = 0; i < GAME_BOARD_DIMENSIONS; i++) {
@@ -65,6 +91,9 @@ public class Printer {
 		System.out.println(" | " + (rowNumber + 1));
 	}
 
+	/**
+	 * Prints a sample coordinates game board
+	 */
 	public void printCoordinatesSampleGameBoard() {
 		System.out.println("\nCoordinates Game Board");
 		System.out.println("\nEnter an x coordinate and a y coordinate to place your move\n");
@@ -80,32 +109,64 @@ public class Printer {
 		}
 	}
 
+	/**
+	 * Prints the top of the coordinates game board
+	 */
 	private void printTopOfCoordinatesGameBoard() {
 		System.out.println("    1   2   3");
 		printCoordinatesGameBoardDivider();
 	}
 
+	/**
+	 * Prints a simple coordinate game board divider
+	 */
 	private void printSimpleCoordinateGameBoardDivider() {
 		System.out.println("|---|---|---|");
 	}
 
+	/**
+	 * Prints a coordinates game board divider
+	 */
 	private void printCoordinatesGameBoardDivider() {
 		System.out.println("  |---|---|---|");
 	}
 
+	/**
+	 * Prints the bottom of a coordinates game board
+	 */
 	private void printBottomOfCoordinatesGameBoard() {
 		printCoordinatesGameBoardDivider();
 		System.out.println("    1   2   3");
 	}
 
+	/**
+	 * Determines if there is a match at the end of a simple row
+	 * 
+	 * @param index the index to check
+	 * @return whether there is a match at the end of a simple row
+	 */
 	private boolean endOfSimpleRowMatch(int index) {
 		return index == 2 || index == 5 || index == 8;
 	}
 
+	/**
+	 * Determines if there is a match at the end of a coordinates row
+	 * 
+	 * @param index the index to check
+	 * @return whether there is a match at the end of a coordinates row
+	 */
 	private boolean endOfRowMatch(int index) {
 		return index != GAME_BOARD_DIMENSIONS - 1;
 	}
 
+	/**
+	 * Prints a game board space
+	 * 
+	 * @param gameBoardSpace the game board space to print
+	 * @param index          the index to check
+	 * 
+	 * @return the game board space to print
+	 */
 	private String printGameBoardSpace(char gameBoardSpace, int index) {
 		if (!helper.isSpaceAlreadyOccupied(gameBoardSpace)) {
 			switch (index) {
