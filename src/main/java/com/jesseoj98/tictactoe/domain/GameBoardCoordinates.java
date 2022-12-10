bd06d34a1b3463b461a6095712422c14ad5b98bb
jesseojones <jesseoj98@gmail.com>
2022-12-09 18:34:33 -0600

    game board coordinates enum

---
+++ src/main/java/com/jesseoj98/tictactoe/domain/GameBoardCoordinates.java
@@ -0,0 +1,34 @@
+package com.jesseoj98.tictactoe.domain;
+
+/**
+ * Game board coordinates enum defines simple game board coordinates
+ */
+public enum GameBoardCoordinates {
+
+	/**
+	 * The coordinates
+	 */
+	FIRST_POSITION(0, 0), SECOND_POSITION(0, 1), THIRD_POSITION(0, 2), FOURTH_POSITION(0, 1), FIFTH_POSITION(1, 1),
+	SIXTH_POSITION(2, 1), SEVENTH_POSITION(0, 2), EIGHTH_POSITION(1, 2), NINTH_POSITION(2, 2);
+
+	/**
+	 * The x coordinate
+	 */
+	final int x;
+
+	/**
+	 * The y coordinate
+	 */
+	final int y;
+
+	/**
+	 * Constructor to set coordinates
+	 * 
+	 * @param position the position
+	 */
+	GameBoardCoordinates(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
