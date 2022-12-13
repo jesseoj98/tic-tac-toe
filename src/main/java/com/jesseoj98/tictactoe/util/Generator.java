8a210afc289485f0de49af13b5191046f110e6d5
jesseojones <jesseoj98@gmail.com>
2022-12-13 11:18:49 -0600

    remove unnecessary list comparison and boilerplate code

--- src/main/java/com/jesseoj98/tictactoe/util/Generator.java
+++ src/main/java/com/jesseoj98/tictactoe/util/Generator.java
@@ -1,8 +1,5 @@
 package com.jesseoj98.tictactoe.util;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import com.jesseoj98.tictactoe.domain.Coordinate;
@@ -29,114 +26,6 @@ public class Generator {
 	 */
 	public char[][] generateCoordinateGameBoard() {
 		return new char[GameBoard.GAME_BOARD_DIMENSION][GameBoard.GAME_BOARD_DIMENSION];
-	}
-
-	/**
-	 * Generates simple coordinates
-	 * 
-	 * @return the simple coordinates
-	 */
-	public List<Integer> generateSimpleCoordinates() {
-		final ArrayList<Integer> simpleCoordinates = new ArrayList<>();
-
-		simpleCoordinates.add(0);
-		simpleCoordinates.add(1);
-		simpleCoordinates.add(2);
-		simpleCoordinates.add(3);
-		simpleCoordinates.add(4);
-		simpleCoordinates.add(5);
-		simpleCoordinates.add(6);
-		simpleCoordinates.add(7);
-		simpleCoordinates.add(8);
-
-		return simpleCoordinates;
-	}
-
-	/**
-	 * Generates simple winning coordinates
-	 * 
-	 * @return the simple winning coordinates
-	 */
-	public List<List<Integer>> generateSimpleWinningCoordinates() {
-		final List<Integer> firstRow = Arrays.asList(0, 1, 2);
-		final List<Integer> secondRow = Arrays.asList(3, 4, 5);
-		final List<Integer> thirdRow = Arrays.asList(6, 7, 8);
-		final List<Integer> firstColumn = Arrays.asList(0, 3, 6);
-		final List<Integer> secondColumn = Arrays.asList(1, 4, 7);
-		final List<Integer> thirdColumn = Arrays.asList(2, 5, 8);
-		final List<Integer> backwardDiagonal = Arrays.asList(0, 4, 8);
-		final List<Integer> forwardDiagonal = Arrays.asList(2, 4, 6);
-
-		final List<List<Integer>> winningCoordinates = new ArrayList<>();
-
-		winningCoordinates.add(firstRow);
-		winningCoordinates.add(secondRow);
-		winningCoordinates.add(thirdRow);
-		winningCoordinates.add(firstColumn);
-		winningCoordinates.add(secondColumn);
-		winningCoordinates.add(thirdColumn);
-		winningCoordinates.add(backwardDiagonal);
-		winningCoordinates.add(forwardDiagonal);
-
-		return winningCoordinates;
-	}
-
-	/**
-	 * Generates coordinates
-	 * 
-	 * @return the coordinates
-	 */
-	public List<Coordinate> generateCoordinates() {
-		final ArrayList<Coordinate> coordinates = new ArrayList<>();
-
-		coordinates.add(new Coordinate(0, 0));
-		coordinates.add(new Coordinate(0, 1));
-		coordinates.add(new Coordinate(0, 2));
-		coordinates.add(new Coordinate(1, 0));
-		coordinates.add(new Coordinate(1, 1));
-		coordinates.add(new Coordinate(1, 2));
-		coordinates.add(new Coordinate(2, 0));
-		coordinates.add(new Coordinate(2, 1));
-		coordinates.add(new Coordinate(2, 2));
-
-		return coordinates;
-	}
-
-	/**
-	 * Generates winning coordinates
-	 * 
-	 * @return the winning coordinates
-	 */
-	public List<List<Coordinate>> generateWinningCoordinates() {
-		final List<Coordinate> firstRow = Arrays.asList(new Coordinate(0, 0), new Coordinate(1, 0),
-				new Coordinate(2, 0));
-		final List<Coordinate> secondRow = Arrays.asList(new Coordinate(0, 1), new Coordinate(1, 1),
-				new Coordinate(2, 1));
-		final List<Coordinate> thirdRow = Arrays.asList(new Coordinate(0, 2), new Coordinate(1, 2),
-				new Coordinate(2, 2));
-		final List<Coordinate> firstColumn = Arrays.asList(new Coordinate(0, 0), new Coordinate(0, 1),
-				new Coordinate(0, 2));
-		final List<Coordinate> secondColumn = Arrays.asList(new Coordinate(1, 0), new Coordinate(1, 1),
-				new Coordinate(1, 2));
-		final List<Coordinate> thirdColumn = Arrays.asList(new Coordinate(2, 0), new Coordinate(2, 1),
-				new Coordinate(2, 2));
-		final List<Coordinate> backwardDiagonal = Arrays.asList(new Coordinate(0, 0), new Coordinate(1, 1),
-				new Coordinate(2, 2));
-		final List<Coordinate> forwardDiagonal = Arrays.asList(new Coordinate(2, 0), new Coordinate(1, 1),
-				new Coordinate(0, 2));
-
-		final List<List<Coordinate>> winningCoordinates = new ArrayList<>();
-
-		winningCoordinates.add(firstRow);
-		winningCoordinates.add(secondRow);
-		winningCoordinates.add(thirdRow);
-		winningCoordinates.add(firstColumn);
-		winningCoordinates.add(secondColumn);
-		winningCoordinates.add(thirdColumn);
-		winningCoordinates.add(backwardDiagonal);
-		winningCoordinates.add(forwardDiagonal);
-
-		return winningCoordinates;
 	}
 
 	/**
