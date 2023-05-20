a8a3e498e27e4f2231436ca0d670d708def1e8d7
jesseojones <jesseoj98@gmail.com>
2023-05-20 18:03:42 -0500

    remove unused method

--- src/main/java/com/jesseoj98/tictactoe/util/helper/inserter/BoardInserter.java
+++ src/main/java/com/jesseoj98/tictactoe/util/helper/inserter/BoardInserter.java
@@ -1,7 +1,5 @@
 package com.jesseoj98.tictactoe.util.helper.inserter;
 
-import com.jesseoj98.tictactoe.domain.Coordinate;
-
 /**
  * Board inserter class provides implementations to insert into game boards
  */
@@ -17,18 +15,6 @@ public class BoardInserter implements GameBoardInserter {
 	@Override
 	public void insertIntoBoard(char[] gameBoard, int userInput, char playingCharacter) {
 		gameBoard[userInput] = playingCharacter;
-	}
-
-	/**
-	 * Inserts a playing character into the coordinate game board
-	 * 
-	 * @param gameBoard        The coordinate game board
-	 * @param coordinate       The coordinate to insert into
-	 * @param playingCharacter The playing character to insert
-	 */
-	@Override
-	public void insertIntoBoard(char[][] gameBoard, Coordinate coordinate, char playingCharacter) {
-		gameBoard[coordinate.getX()][coordinate.getY()] = playingCharacter;
 	}
 
 	/**
--- src/main/java/com/jesseoj98/tictactoe/util/helper/inserter/GameBoardInserter.java
+++ src/main/java/com/jesseoj98/tictactoe/util/helper/inserter/GameBoardInserter.java
@@ -1,15 +1,11 @@
 package com.jesseoj98.tictactoe.util.helper.inserter;
 
-import com.jesseoj98.tictactoe.domain.Coordinate;
-
 /**
  * Game board inserter interface supplies blueprints to insert into a game board
  */
 public interface GameBoardInserter {
 
 	public void insertIntoBoard(char[] gameBoard, int userInput, char playingCharacter);
-
-	public void insertIntoBoard(char[][] gameBoard, Coordinate coordinate, char playingCharacter);
 
 	public void insertIntoBoard(char[][] gameBoard, int xCoordinate, int yCoordinate, char playingCharacter);
 
