13d22632d3e24a518fb535740f7d30481c2841af
jesseoj98 <jesseoj98@gmail.com>
2023-07-04 13:50:33 -0500

    enhanced switch expression per intellij

--- src/main/java/com/jesseoj98/tictactoe/util/printer/space/SpacePrinter.java
+++ src/main/java/com/jesseoj98/tictactoe/util/printer/space/SpacePrinter.java
@@ -8,28 +8,18 @@ public class SpacePrinter {
 
     public String printSpace(char space, int index) {
         if (!bo.isSpaceOccupied(space)) {
-            switch (index) {
-                case 0:
-                    return "1";
-                case 1:
-                    return "2";
-                case 2:
-                    return "3";
-                case 3:
-                    return "4";
-                case 4:
-                    return "5";
-                case 5:
-                    return "6";
-                case 6:
-                    return "7";
-                case 7:
-                    return "8";
-                case 8:
-                    return "9";
-                default:
-                    return " ";
-            }
+            return switch (index) {
+                case 0 -> "1";
+                case 1 -> "2";
+                case 2 -> "3";
+                case 3 -> "4";
+                case 4 -> "5";
+                case 5 -> "6";
+                case 6 -> "7";
+                case 7 -> "8";
+                case 8 -> "9";
+                default -> " ";
+            };
         } else {
             return Character.toString(space);
         }
