6ec1d80ecbbf0672f0fdc77bb28fe7309cf8a1d1
jesseoj98 <jesseoj98@gmail.com>
2023-07-04 16:40:54 -0500

    code clean-up per intellij for tic-tac-toe validator class

--- src/main/java/com/jesseoj98/tictactoe/util/helper/validator/tictactoe/TicTacToeValidator.java
+++ src/main/java/com/jesseoj98/tictactoe/util/helper/validator/tictactoe/TicTacToeValidator.java
@@ -3,25 +3,19 @@ package com.jesseoj98.tictactoe.util.helper.validator.tictactoe;
 public class TicTacToeValidator {
 
 	public boolean ticTacToe(char[] gameBoard, char playingCharacter) {
-		final String ticTacToe = new StringBuilder().append(playingCharacter).append(playingCharacter)
-				.append(playingCharacter).toString();
+		final String ticTacToe = String.valueOf(playingCharacter) + playingCharacter +
+				playingCharacter;
 
-		final String topRow = new StringBuilder().append(gameBoard[0]).append(gameBoard[1]).append(gameBoard[2])
-				.toString();
-		final String middleRow = new StringBuilder().append(gameBoard[3]).append(gameBoard[4]).append(gameBoard[5])
-				.toString();
-		final String bottomRow = new StringBuilder().append(gameBoard[6]).append(gameBoard[7]).append(gameBoard[8])
-				.toString();
-		final String leftColumn = new StringBuilder().append(gameBoard[0]).append(gameBoard[3]).append(gameBoard[6])
-				.toString();
-		final String middleColumn = new StringBuilder().append(gameBoard[1]).append(gameBoard[4]).append(gameBoard[7])
-				.toString();
-		final String rightColumn = new StringBuilder().append(gameBoard[2]).append(gameBoard[5]).append(gameBoard[8])
-				.toString();
-		final String backwardDiagonal = new StringBuilder().append(gameBoard[0]).append(gameBoard[4])
-				.append(gameBoard[8]).toString();
-		final String forwardDiagonal = new StringBuilder().append(gameBoard[2]).append(gameBoard[4])
-				.append(gameBoard[6]).toString();
+		final String topRow = String.valueOf(gameBoard[0]) + gameBoard[1] + gameBoard[2];
+		final String middleRow = String.valueOf(gameBoard[3]) + gameBoard[4] + gameBoard[5];
+		final String bottomRow = String.valueOf(gameBoard[6]) + gameBoard[7] + gameBoard[8];
+		final String leftColumn = String.valueOf(gameBoard[0]) + gameBoard[3] + gameBoard[6];
+		final String middleColumn = String.valueOf(gameBoard[1]) + gameBoard[4] + gameBoard[7];
+		final String rightColumn = String.valueOf(gameBoard[2]) + gameBoard[5] + gameBoard[8];
+		final String backwardDiagonal = String.valueOf(gameBoard[0]) + gameBoard[4] +
+				gameBoard[8];
+		final String forwardDiagonal = String.valueOf(gameBoard[2]) + gameBoard[4] +
+				gameBoard[6];
 
 		return ticTacToe.equals(topRow) || ticTacToe.equals(middleRow) || ticTacToe.equals(bottomRow)
 				|| ticTacToe.equals(leftColumn) || ticTacToe.equals(middleColumn) || ticTacToe.equals(rightColumn)
@@ -29,25 +23,25 @@ public class TicTacToeValidator {
 	}
 
 	public boolean ticTacToe(char[][] gameBoard, char playingCharacter) {
-		final String ticTacToe = new StringBuilder().append(playingCharacter).append(playingCharacter)
-				.append(playingCharacter).toString();
+		final String ticTacToe = String.valueOf(playingCharacter) + playingCharacter +
+				playingCharacter;
 
-		final String topRow = new StringBuilder().append(gameBoard[0][0]).append(gameBoard[0][1])
-				.append(gameBoard[0][2]).toString();
-		final String middleRow = new StringBuilder().append(gameBoard[1][0]).append(gameBoard[1][1])
-				.append(gameBoard[1][2]).toString();
-		final String bottomRow = new StringBuilder().append(gameBoard[2][0]).append(gameBoard[2][1])
-				.append(gameBoard[2][2]).toString();
-		final String leftColumn = new StringBuilder().append(gameBoard[0][0]).append(gameBoard[1][0])
-				.append(gameBoard[2][0]).toString();
-		final String middleColumn = new StringBuilder().append(gameBoard[0][1]).append(gameBoard[1][1])
-				.append(gameBoard[2][1]).toString();
-		final String rightColumn = new StringBuilder().append(gameBoard[0][2]).append(gameBoard[1][2])
-				.append(gameBoard[2][2]).toString();
-		final String backwardDiagonal = new StringBuilder().append(gameBoard[0][0]).append(gameBoard[1][1])
-				.append(gameBoard[2][2]).toString();
-		final String forwardDiagonal = new StringBuilder().append(gameBoard[2][0]).append(gameBoard[1][1])
-				.append(gameBoard[0][2]).toString();
+		final String topRow = String.valueOf(gameBoard[0][0]) + gameBoard[0][1] +
+				gameBoard[0][2];
+		final String middleRow = String.valueOf(gameBoard[1][0]) + gameBoard[1][1] +
+				gameBoard[1][2];
+		final String bottomRow = String.valueOf(gameBoard[2][0]) + gameBoard[2][1] +
+				gameBoard[2][2];
+		final String leftColumn = String.valueOf(gameBoard[0][0]) + gameBoard[1][0] +
+				gameBoard[2][0];
+		final String middleColumn = String.valueOf(gameBoard[0][1]) + gameBoard[1][1] +
+				gameBoard[2][1];
+		final String rightColumn = String.valueOf(gameBoard[0][2]) + gameBoard[1][2] +
+				gameBoard[2][2];
+		final String backwardDiagonal = String.valueOf(gameBoard[0][0]) + gameBoard[1][1] +
+				gameBoard[2][2];
+		final String forwardDiagonal = String.valueOf(gameBoard[2][0]) + gameBoard[1][1] +
+				gameBoard[0][2];
 
 		return ticTacToe.equals(topRow) || ticTacToe.equals(middleRow) || ticTacToe.equals(bottomRow)
 				|| ticTacToe.equals(leftColumn) || ticTacToe.equals(middleColumn) || ticTacToe.equals(rightColumn)
