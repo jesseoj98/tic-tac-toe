3092c41c445f907f6068ec2f1cdc73926cbc4fab
jesseoj98 <jesseoj98@gmail.com>
2023-12-12 17:52:58 -0600

    re-work actual coordinate board printer

--- src/main/java/com/jesseoj98/tictactoe/util/printer/board/actual/ActualCoordinateBoardPrinter.java
+++ src/main/java/com/jesseoj98/tictactoe/util/printer/board/actual/ActualCoordinateBoardPrinter.java
@@ -11,15 +11,12 @@ public class ActualCoordinateBoardPrinter {
 	private static final BoardStructurePrinter boardStructurePrinter = new BoardStructurePrinter();
 
 	public void printActualBoardSequence(char[][] gameBoard) {
-		boardStructurePrinter.printNumbers(4, 3, true);
 		printActualBoard(gameBoard);
-		boardStructurePrinter.printNumbers(4, 3, true);
 	}
 
 	private void printActualBoard(char[][] gameBoard) {
-		boardStructurePrinter.printBoardDivider(2, 3);
+		boardStructurePrinter.printGameBoardDivider(3);
 		for (int i = 0; i < GameBoard.GAME_BOARD_DIMENSION; i++) {
-			System.out.print((i + 1) + " ");
 			for (int j = 0; j < GameBoard.GAME_BOARD_DIMENSION; j++) {
 				if (boardOccupier.isSpaceOccupied(gameBoard[i][j])) {
 					System.out.print("| " + gameBoard[i][j] + " ");
@@ -27,11 +24,12 @@ public class ActualCoordinateBoardPrinter {
 					System.out.print("|" + (j + 1) + "," + (i + 1));
 				}
 			}
-			System.out.println("| " + (i + 1));
+			System.out.print("| " + (i + 1));
 			if (i != GameBoard.GAME_BOARD_DIMENSION - 1) {
-				boardStructurePrinter.printBoardDivider(2, 3);
+				boardStructurePrinter.printGameBoardDivider(3);
 			}
 		}
-		boardStructurePrinter.printBoardDivider(2, 3);
+		boardStructurePrinter.printGameBoardDivider(3);
+		boardStructurePrinter.printHorizontalEdgeIndices(3);
 	}
 }
--- src/main/java/com/jesseoj98/tictactoe/util/printer/board/structure/BoardStructurePrinter.java
+++ src/main/java/com/jesseoj98/tictactoe/util/printer/board/structure/BoardStructurePrinter.java
@@ -1,38 +1,17 @@
 package com.jesseoj98.tictactoe.util.printer.board.structure;
 
 public class BoardStructurePrinter {
-
-	public void printBoardDivider(int spaces, int columns) {
-		printSpaces(spaces);
-		printColumnsOrNumbers(columns, true, false);
-	}
-
-	public void printNumbers(int spaces, int numberOfNumbers, boolean printAdditionalSpaces) {
-		printSpaces(spaces);
-		printColumnsOrNumbers(numberOfNumbers, false, printAdditionalSpaces);
-	}
-
-	private void printColumnsOrNumbers(int timesToPrint, boolean printColumns, boolean printAdditionalSpaces) {
-		for (int i = 0; i < timesToPrint; i++) {
-			System.out.print(printColumns ? "|---" : (i + 1));
-			System.out.print(printAdditionalSpaces ? "   " : "");
-			if (printAdditionalSpaces && i == timesToPrint - 1) {
+	
+	public void printHorizontalEdgeIndices(int indices) {
+		System.out.print("  ");
+		for (int i = 1; i <= indices; i++) {
+			System.out.print(i);
+			if (i == indices) { // just to not append useless spaces
 				break;
 			}
-			if (i == timesToPrint - 1) {
-				System.out.print(printColumns ? "|" : timesToPrint);
-				break;
-			}
+			System.out.print("   ");
 		}
 		System.out.println();
-	}
-
-	private void printSpaces(int spaces) {
-		if (spaces != 0) {
-			for (int i = 0; i < spaces; i++) {
-				System.out.print(" ");
-			}
-		}
 	}
 
 	public void printGameBoardDivider(int columns) {
